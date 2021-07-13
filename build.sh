c7f797f3be6df2c8d191db61b7aae0a79bf54251
Jae Bradley <jae.b.bradley@gmail.com>
2021-07-13 00:03:00 -0400

    Update

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ if [[ $? -ne 0 ]]; then echo "git command does not exist" && exit 255; fi
 cd "$HOME"
 if [[ $? -ne 0 ]]; then echo "unable to move to $HOME directory" && exit 255; fi
 
-git clone https://github.com/jaebradley/dotfiles.git .dotfiles
+git clone --bare https://github.com/jaebradley/dotfiles.git .dotfiles
 if [[ $? -ne 0 ]]; then echo "could not clone dotfiles repository" && exit 255; fi
 
-/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
 if [[ $? -ne 0 ]]; then echo "unable to checkout config" && exit 255; fi
 
 if [[ -x "./scripts/start.sh" ]]; then echo "start script is not executable" && exit 255; fi
